83c6409d03fa010e49c8158ced04a30cf06837ef
Valdas <valdas.rapsevicius@mif.vu.lt>
2019-04-16 14:27:21 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'ml_examples',
+    version = '0.0.1',
+    url = 'https://github.com/valdasraps/ml_examples.git',
+    author = 'Valdas Rapsevicius',
+    author_email = 'valdas.rapsevicius@mif.vu.lt',
+    description = 'ML examples for Data Science',
+    packages = find_packages(),
+    install_requires = [],
+)
